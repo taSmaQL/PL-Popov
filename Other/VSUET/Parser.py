b6742b466d3c9fa831c5ce3776e9a54a039cfe6d
KirillVSUET-20 <kosmosmakmelov4@gmail.com>
2025-02-25 19:14:55 +0300

    Update Parser.py

--- Other/VSUET/Parser.py
+++ Other/VSUET/Parser.py
@@ -6,7 +6,7 @@ import re
 import json
 HOST = 'https://vsuet.ru'
 URL = 'https://vsuet.ru/obuchenie/faculties'
-
+--Заброшено
 HEADERS = {
       'user-agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36"
 }
